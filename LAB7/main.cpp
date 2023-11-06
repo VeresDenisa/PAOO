d88db82a4c65ba2443daf9c3e3e66b34fd04d9aa
denisa <denisa@Ubuntu.myguest.virtualbox.org>
2023-11-06 11:36:03 +0200

    Lab 7 - Namespace example

--- LAB7/main.cpp
+++ LAB7/main.cpp
@@ -2,11 +2,21 @@
 
 #include <iostream>
 
+namespace firstBookStore{
+    void display(){
+        std::cout<<"FIRST namespace"<<std::endl;
+    }
+}
+
+using namespace firstBookStore;
+
 int main(int argc, char* argv[])
 {
     Book noapte(1879, (char*)"O noapte furtunoasa", (char*)"I.L.Caragiale", 34.89, false);
     Book enigma(Book(1938, (char*)"Enigma Otiliei", (char*)"George Calinescu", 50.00, true));
     Book morometi(1955, (char*)"Morometii", (char*)"Marin Preda", 25.00, true);
+    
+    firstBookStore::display();
     
     noapte.display();
     enigma.display();
